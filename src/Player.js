927caae9672f2433160c8eb191b785c82609045f
vilgjoy <vilgot.sandberg@elev.ga.ntig.se>
2025-12-05 11:19:28 +0100

    lektion 12-05. gjort glad och ledsen mun

--- src/Player.js
+++ src/Player.js
@@ -13,6 +13,10 @@ export default class Player extends GameObject {
         this.moveSpeed = 0.5
         this.directionX = 0
         this.directionY = 0
+
+        this.idleTimer = 0
+        // tiden (i millisekunder) innan spelaren blir ledsen
+        this.sadTime = 4000
     }
 
     update(deltaTime) {
@@ -37,6 +41,14 @@ export default class Player extends GameObject {
         } else {
             this.velocityX = 0
             this.directionX = 0
+        }
+
+        // om man rör sig (hastigheten inte 0), nollställ timer och bli glad
+        if (this.velocityX !== 0 || this.velocityY !== 0) {
+            this.idleTimer = 0
+        } else {
+            // Om vi står still, lägg till tid till timern
+            this.idleTimer += deltaTime
         }
 
         // Uppdatera position baserat på hastighet
@@ -68,12 +80,26 @@ export default class Player extends GameObject {
             this.width * 0.1, 
             this.height * 0.1
         )
-        // rita mun som ett streck
+
         ctx.strokeStyle = 'black'
         ctx.lineWidth = 2
         ctx.beginPath()
-        ctx.moveTo(this.x + this.width * 0.3, this.y + this.height * 0.65)
-        ctx.lineTo(this.x + this.width * 0.7, this.y + this.height * 0.65)
+
+        const mouthCenterX = this.x + this.width * 0.5
+        // sätter Y lite olika beroende på om den är glad eller ledsen för att det ska se snyggt ut
+        let mouthCenterY = this.y + this.height * 0.65 
+        const mouthRadius = this.width * 0.2
+
+        if (this.idleTimer > this.sadTime) {
+            // ledsen: ritar en båge moturs (ser ut som ett uppochnervänt U)
+            // flyttar ner centern lite så bågen inte hamnar mitt i ansiktet
+            mouthCenterY = this.y + this.height * 0.8
+            ctx.arc(mouthCenterX, mouthCenterY, mouthRadius, 0, Math.PI, true) 
+        } else {
+            // glad: ritar en båge medurs (ser ut som ett U)
+            ctx.arc(mouthCenterX, mouthCenterY, mouthRadius, 0, Math.PI, false)
+        }
+        
         ctx.stroke()
     }
 }
